c99e6d7d300f1e8cbdf4ebfbad34747327a94e01
User <User@DESKTOP-2RC2UP5>
2019-09-25 09:01:43 +0700

    first commit

---
+++ src/latihan13/Latihan13.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan13;
+
+/**
+ *
+ * @author User
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ * 
+ * 
+ */
+public class Latihan13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 =10;
+        int bil2 =20;
+        System.out.println("a==b="+(bil1==bil2));
+          System.out.println("a!=b="+(bil1!=bil2));
+          System.out.println("a>b="+(bil1>bil2));
+            System.out.println("a<b="+(bil1<bil2));
+              System.out.println("a>=b="+(bil2==bil1));
+                System.out.println("a<=b="+(bil2<=bil1));
+    }
+    
+}
